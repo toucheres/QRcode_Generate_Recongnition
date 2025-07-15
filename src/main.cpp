9fc793afeef910855630050ed2fb7ed125590c12
toucheres <761844639@qq.com>
2025-07-15 15:30:03 +0800

    add icon

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,13 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-    
-    // 设置默认编码为UTF-8
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
-#endif
-    
-    // 设置应用程序信息
+    QApplication app(argc, argv);   
     app.setApplicationName("QRcode生成识别器");
     app.setApplicationVersion("1.0");
-    app.setOrganizationName("MyCompany");
-    
+    app.setOrganizationName("SCU-CS");
+    app.setWindowIcon(QIcon{":/toucher.jpg"});
     // 创建并显示主窗口
     MainWindow window;
     window.show();
-    
     return app.exec();
 }
